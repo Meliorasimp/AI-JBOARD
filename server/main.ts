8b0693c1902436c2d4aa9932b261605e85aa7c3c
Meinard Legashki Limlengco <meliorasimp@gmail.com>
2025-07-02 20:08:52 +0800

    Completed Tailwind css Setup

--- server/main.ts
+++ server/main.ts
@@ -2,9 +2,8 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 const PORT = 3000;
-
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from TypeScript + Express!');
+  res.send(`Hello from TypeScript + Express!`);
 });
 
 app.listen(PORT, () => {
